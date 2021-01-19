7a51c4175e5c63caf0b8731cddd9451404648a6a
cortelucas <corte1994.lc@gmail.com>
2021-01-19 16:11:00 -0300

    - implementações de validacoes;

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,17 +5,40 @@ class Atendimento {
     adiciona(atendimento) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-        const atendimentoDatado = {...atendimento, dataCriacao, data };
 
-        const sql = 'INSERT INTO Atendimentos SET ?';
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                resultados.status(400).json(erro);
-            } else {
-                resultados.status(201).json(resultados);
+        const validacoes = [{
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter ao mesmo 5 caractéres'
             }
-        });
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if (existemErros) {
+            res.status(400).json();
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data };
+
+            const sql = 'INSERT INTO Atendimentos SET ?';
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    resultados.status(400).json(erro);
+                } else {
+                    resultados.status(201).json(resultados);
+                }
+            });
+        }
     }
 }
 
